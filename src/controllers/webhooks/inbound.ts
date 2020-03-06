427374986ac4c36692ce91487071c020ecd6acfc
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-06 20:38:17 +0100

    :sparkles: Add inbound email endpoint

---
+++ src/controllers/webhooks/inbound.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Request, Response } from "@staart/server";
+import { joiValidate, Joi } from "@staart/validate";
+import { respond, RESOURCE_SUCCESS } from "@staart/messages";
+
+@Controller("inbound")
+export class WebhooksController {
+  @Get("email/:id")
+  async inboundEmail(req: Request) {
+    const id = req.params.id;
+    const secret = (req.get("Authorization") || "").replace("Bearer ", "");
+    joiValidate(
+      {
+        id: Joi.string().required(),
+        secret: Joi.string().required()
+      },
+      { id, secret }
+    );
+    console.log("Incoming email", id);
+    return respond(RESOURCE_SUCCESS);
+  }
+}
