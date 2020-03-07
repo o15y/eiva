87ae655e415cd259aa3cb74af53fafe37ca4da6c
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-07 15:25:37 +0100

    :recycle: Export entity types for ease

--- src/helpers/services/ara/actions/setupNewAppointment.ts
+++ src/helpers/services/ara/actions/setupNewAppointment.ts
@@ -2,8 +2,25 @@ import { ActionParams } from "../../../../interfaces/ara";
 import { detectEntities } from "../../google-cloud";
 
 export const setupNewAppointment = async (params: ActionParams) => {
-  const { entities, language } = await detectEntities(params.tokens.join(". "));
-  params.log("Found entities", entities);
+  const {
+    persons,
+    locations,
+    organizations,
+    events,
+    consumerGoods,
+    phoneNumbers,
+    addresses,
+    dates,
+    language
+  } = await detectEntities(params.tokens.join(". "));
   params.log(`Detected language as "${language}"`);
+  if (persons.length) params.log("Detected persons", persons);
+  if (locations.length) params.log("Detected locations", locations);
+  if (organizations.length) params.log("Detected organizations", organizations);
+  if (events.length) params.log("Detected events", events);
+  if (consumerGoods.length) params.log("Detected consumerGoods", consumerGoods);
+  if (phoneNumbers.length) params.log("Detected phoneNumbers", phoneNumbers);
+  if (addresses.length) params.log("Detected addresses", addresses);
+  if (dates.length) params.log("Detected dates", dates);
   throw new Error("I don't know how to set up a new appointment");
 };
--- src/helpers/services/google-cloud.ts
+++ src/helpers/services/google-cloud.ts
@@ -1,9 +1,39 @@
 import axios from "axios";
 import { Entity } from "../../interfaces/google-cloud";
+import { readJson, writeJson } from "fs-extra";
+import { join } from "path";
+import { createHash } from "crypto";
 
-export const detectEntities = async (text: string) => {
+export const detectEntities = async (
+  text: string
+): Promise<{
+  language: string;
+  entities: Entity[];
+  numbers: Entity[];
+  unknowns: Entity[];
+  persons: Entity[];
+  locations: Entity[];
+  organizations: Entity[];
+  events: Entity[];
+  workOfArts: Entity[];
+  consumerGoods: Entity[];
+  others: Entity[];
+  phoneNumbers: Entity[];
+  addresses: Entity[];
+  dates: Entity[];
+  prices: Entity[];
+}> => {
+  const KEY = `googlecloud${createHash("md5")
+    .update(text)
+    .digest("hex")}.json`;
+  if (process.env.NODE_ENV === "development") {
+    try {
+      const file = await readJson(join(".", ".cache", KEY));
+      if (file) return file;
+    } catch (error) {}
+  }
   try {
-    return (
+    const data = (
       await axios.post<{
         language: string;
         entities: Entity[];
@@ -19,6 +49,26 @@ export const detectEntities = async (text: string) => {
         {}
       )
     ).data;
+    const result: any = { ...data };
+    result.numbers = data.entities.filter(i => i.type === "NUMBER");
+    result.unknowns = data.entities.filter(i => i.type === "UNKNOWN");
+    result.persons = data.entities.filter(i => i.type === "PERSON");
+    result.locations = data.entities.filter(i => i.type === "LOCATION");
+    result.organizations = data.entities.filter(i => i.type === "ORGANIZATION");
+    result.events = data.entities.filter(i => i.type === "EVENT");
+    result.workOfArts = data.entities.filter(i => i.type === "WORK_OF_ART");
+    result.consumerGoods = data.entities.filter(
+      i => i.type === "CONSUMER_GOOD"
+    );
+    result.others = data.entities.filter(i => i.type === "OTHER");
+    result.phoneNumbers = data.entities.filter(i => i.type === "PHONE_NUMBER");
+    result.addresses = data.entities.filter(i => i.type === "ADDRESS");
+    result.dates = data.entities.filter(i => i.type === "DATE");
+    result.prices = data.entities.filter(i => i.type === "PRICE");
+    if (process.env.NODE_ENV === "development") {
+      await writeJson(join(".", ".cache", KEY), result);
+    }
+    return result;
   } catch (error) {
     throw new Error("Unable to detect entities in text");
   }
