5096dac9dddbd3c4a946a779687c684c3ef5ce32
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-08 04:16:43 +0530

    :bug: Make sure .cache dir exists

--- src/ara/services/s3.ts
+++ src/ara/services/s3.ts
@@ -1,5 +1,5 @@
 import AWS from "aws-sdk";
-import { readFile, writeFile } from "fs-extra";
+import { mkdirp, readFile, writeFile } from "fs-extra";
 import { join } from "path";
 
 const S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID || "";
@@ -10,7 +10,7 @@ const s3 = new AWS.S3({
   apiVersion: "2006-03-01",
   accessKeyId: S3_ACCESS_KEY_ID,
   secretAccessKey: S3_SECRET_ACCESS_KEY,
-  region: S3_REGION
+  region: S3_REGION,
 });
 
 export const getS3Item = async (bucketName: string, objectId: string) => {
@@ -23,6 +23,7 @@ export const getS3Item = async (bucketName: string, objectId: string) => {
   }
   const result = await safeGetS3Item(bucketName, objectId);
   if (process.env.NODE_ENV === "development") {
+    await mkdirp(join(".", ".cache"));
     await writeFile(join(".", ".cache", KEY), result.toString());
   }
   return result;
@@ -33,7 +34,7 @@ const safeGetS3Item = (bucketName: string, objectId: string): Promise<Buffer> =>
     s3.getObject(
       {
         Bucket: bucketName,
-        Key: objectId
+        Key: objectId,
       },
       (error, data) => {
         if (error) return reject(error);
--- src/controllers/webhooks/inbound.ts
+++ src/controllers/webhooks/inbound.ts
@@ -5,11 +5,14 @@ import { Joi, joiValidate } from "@staart/validate";
 export class InboundWebhooksController {
   @Get("email/:objectId")
   async lambdaWebhook(req: Request, res: Response) {
-    const secret = (req.get("Authorization") || "").replace("Bearer ", "");
+    const secret =
+      (req.get("Authorization") || "").replace("Bearer ", "") ||
+      req.query.secret;
     joiValidate(
       { secret: Joi.string().required(), objectId: Joi.string().required() },
       { secret, objectId: req.params.objectId }
     );
-    return processIncomingEmail(secret, req.params.objectId);
+    processIncomingEmail(secret, req.params.objectId).catch(() => {});
+    return { queued: true };
   }
 }
