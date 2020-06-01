2e8bbe83cacf55fe298c6a8cd28acc863525b092
Anand Chowdhary <mail@anandchowdhary.com>
2020-06-01 21:12:50 +0530

    :sparkles: Make sure location is deletable

--- src/ara/services/crud/locations.ts
+++ src/ara/services/crud/locations.ts
@@ -6,10 +6,7 @@ import {
 import { can } from "../../../_staart/helpers/authorization";
 import { OrgScopes } from "../../../_staart/interfaces/enum";
 import { locationsUpdateInput, locationsCreateInput } from "@prisma/client";
-import {
-  INSUFFICIENT_PERMISSION,
-  CANNOT_DELETE_SOLE_MEMBER,
-} from "@staart/errors";
+import { INSUFFICIENT_PERMISSION, RESOURCE_NOT_FOUND } from "@staart/errors";
 import { ApiKeyResponse } from "../../../_staart/helpers/jwt";
 
 export const getAllLocationsForOrganization = async (
@@ -84,10 +81,48 @@ export const deleteLocationForOrganization = async (
   if (
     await can(tokenUserId, OrgScopes.UPDATE_ORG, "organization", organizationId)
   ) {
+    /**
+     * Make sure at least 2 locations exist
+     * You cannot delete your location without a backup location
+     */
     const locations = await prisma.locations.findMany({
       where: { organizationId: parseInt(organizationId) },
+      take: 2,
     });
-    if (locations.length === 1) throw new Error(CANNOT_DELETE_SOLE_MEMBER);
+    if (locations.length === 1)
+      throw new Error("400/cannot-delete-sole-location");
+
+    /** Make sure this location exists */
+    const locationDetails = await prisma.locations.findOne({
+      where: { id: parseInt(meetingId) },
+    });
+    if (!locationDetails) throw new Error(RESOURCE_NOT_FOUND);
+
+    /** Find primary location for this team */
+    const primaryLocationId = await prisma.organizations.findOne({
+      where: { id: parseInt(organizationId) },
+      select: { schedulingLocation: true },
+    });
+
+    /** If this is the primary location, set another one */
+    if (primaryLocationId.schedulingLocation === locationDetails.id) {
+      const anotherLocation = await prisma.locations.findMany({
+        take: 1,
+        where: {
+          organizationId: parseInt(organizationId),
+          id: { not: locationDetails.id },
+        },
+        orderBy: { id: "desc" },
+      });
+      if (!anotherLocation.length)
+        throw new Error("400/cannot-delete-sole-location");
+      await prisma.organizations.update({
+        where: { id: parseInt(organizationId) },
+        data: { schedulingLocation: anotherLocation[0].id },
+      });
+    }
+
+    /** Finally, delete this location */
     return prisma.locations.delete({ where: { id: parseInt(meetingId) } });
   }
   throw new Error(INSUFFICIENT_PERMISSION);
